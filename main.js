7c53b1987aed7a97090cbd2dfabc58690156918d
Mathijs Dheedene <101631340+MetMaxPro@users.noreply.github.com>
2025-06-02 14:27:36 +0200

    Update main.js feed x3

--- main.js
+++ main.js
@@ -51,46 +51,46 @@ function provideFeedback() {
   if (amount < 50) {
     switch (category) {
       case 'Onderwijs, Cultuur en Wetenschap':
-        feedback += `<li style="color: red;">Onderwijs, Cultuur en Wetenschap zijn ondergefinancierd. Dit kan de kwaliteit van het onderwijs be&#xefnvloeden.</li>`;
+        feedback += `<li style="color: red;"><strong>Onderwijs, Cultuur en Wetenschap</strong> zijn ondergefinancierd. Dit kan de kwaliteit van het onderwijs be&#xefnvloeden.</li><br>`;
         break;
       case 'Zorg':
-        feedback += `<li style="color: red;">Zorg krijgt te weinig. Dit kan leiden tot langere wachttijden en verminderde zorg.</li>`;
+        feedback += `<li style="color: red;"><strong>Zorg</strong> krijgt te weinig. Dit kan leiden tot langere wachttijden en verminderde zorg.</li><br>`;
         break;
       case 'Sociale Zekerheid':
-        feedback += `<li style="color: red;">Sociale Zekerheid krijgt te weinig. Dit kan leiden tot lagere uitkeringen en minder ondersteuning voor kwetsbare groepen.</li>`;
+        feedback += `<li style="color: red;"><strong>Sociale Zekerheid</strong> krijgt te weinig. Dit kan leiden tot lagere uitkeringen en minder ondersteuning voor kwetsbare groepen.</li><br>`;
         break;
       case 'Gemeente- en Provinciefonds':
-        feedback += `<li style="color: red;">De Gemeenten en Provincies krijgen te weinig. Dit kan leiden tot achterstallig onderhoud en verminderde lokale dienstverlening.</li>`;
+        feedback += `<li style="color: red;">De <strong>Gemeenten en Provincies</strong> krijgen te weinig. Dit kan leiden tot achterstallig onderhoud en verminderde lokale dienstverlening.</li><br>`;
         break;
       case 'Defensie, Justitie en Veiligheid':
-        feedback += `<li style="color: red;">Defensie, Justitie en Veiligheid zijn ondergefinancierd. Dit kan nationale veiligheid be&#xefnvloeden.</li>`;
+        feedback += `<li style="color: red;"><strong>Defensie, Justitie en Veiligheid</strong> zijn ondergefinancierd. Dit kan nationale veiligheid be&#xefnvloeden.</li><br>`;
         break;
       default:
-        feedback += `<li style="color: red;">${category} heeft te weinig middelen. Dit kan leiden tot problemen.</li>`;
+        feedback += `<li style="color: red;"><strong>${category}</strong> heeft te weinig middelen. Dit kan leiden tot problemen.</li><br>`;
     }
   } else if (amount > 130) {
     color = 'green';
     switch (category) {
      case 'Onderwijs, Cultuur en Wetenschap':
-        feedback += `<li style="color: green;">Onderwijs, Cultuur en Wetenschap krijgen veel middelen. Dit biedt kansen voor innovatie en betere faciliteiten.</li>`;
+        feedback += `<li style="color: green;"><strong>Onderwijs, Cultuur en Wetenschap</strong> krijgen veel middelen. Dit biedt kansen voor innovatie en betere faciliteiten.</li><br>`;
         break;
       case 'Zorg':
-        feedback += `<li style="color: green;">Zorg heeft ruim voldoende middelen. Dit verhoogt de kwaliteit van zorg.</li>`;
+        feedback += `<li style="color: green;"><strong>Zorg</strong> heeft ruim voldoende middelen. Dit verhoogt de kwaliteit van zorg.</li><br>`;
         break;
       case 'Sociale Zekerheid':
-        feedback += `<li style="color: green;">Sociale Zekerheid krijgt ruim voldoende middelen. Dit versterkt de bestaanszekerheid, maar de hoge uitgaven moeten wel op lange termijn houdbaar blijven.</li>`;
+        feedback += `<li style="color: green;"><strong>Sociale Zekerheid</strong> krijgt ruim voldoende middelen. Dit versterkt de bestaanszekerheid, maar de hoge uitgaven moeten wel op lange termijn houdbaar blijven.</li><br>`;
         break;
       case 'Gemeente- en Provinciefonds':
-        feedback += `<li style="color: green;">De Gemeenten en Provincies ontvangen veel middelen. Dit kan zorgen voor verbeterde infrastructuur en extra lokale voorzieningen, maar kan ook leiden tot ineffici&#235nte besteding</li>`;
+        feedback += `<li style="color: green;">De <strong>Gemeenten en Provincies</strong> ontvangen veel middelen. Dit kan zorgen voor verbeterde infrastructuur en extra lokale voorzieningen, maar kan ook leiden tot ineffici&#235nte besteding</li><br>`;
         break;
       case 'Defensie, Justitie en Veiligheid':
-        feedback += `<li style="color: green;">Defensie, Justitie en Veiligheid krijgen veel. Dit versterkt de nationale veiligheid, maar gaat mogelijk ten koste van andere sectoren.</li>`;
+        feedback += `<li style="color: green;"><strong>Defensie, Justitie en Veiligheid</strong> krijgen veel. Dit versterkt de nationale veiligheid, maar gaat mogelijk ten koste van andere sectoren.</li><br>`;
         break;
       default:
-        feedback += `<li style="color: green;">${category} heeft veel middelen gekregen. Dit is positief, maar het kan tekorten veroorzaken in andere sectoren.</li>`;
+        feedback += `<li style="color: green;"><strong>${category}</strong> heeft veel middelen gekregen. Dit is positief, maar het kan tekorten veroorzaken in andere sectoren.</li><br>`;
     }
   } else {
-    feedback += `<li style="color: black;">${category} heeft een gebalanceerd budget.</li>`;
+    feedback += `<li style="color: black;"><strong>${category}</strong> heeft een gebalanceerd budget.</li><br>`;
   }
 
 });
